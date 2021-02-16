3a47833660258a3b83a9e8945bf54ad91b5dc440
Meg <meganriley806@gmail.com>
2021-02-16 09:25:49 -0600

    Completed module

--- ssg/hooks.py
+++ ssg/hooks.py
@@ -8,5 +8,5 @@ def register(hook, order=0):
 
 def event(hook, *args):
     for order in sorted(_callbacks.get(hook, {})):
-        for func in _callbacks[hook]:
+        for func in _callbacks[hook][order]:
             func(*args)
